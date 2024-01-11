ea169b1da0a460a155313d6a2e02e65c0d415a36
Hongyu Chiu <20734616+james77777778@users.noreply.github.com>
2024-01-11 22:27:27 +0800

    Add efficientnet lite

---
+++ kimm/models/efficientnet_lite.py
@@ -0,0 +1,500 @@
+import math
+import typing
+
+import keras
+from keras import backend
+from keras import layers
+from keras import utils
+from keras.src.applications import imagenet_utils
+
+from kimm.blocks import apply_conv2d_block
+from kimm.models.feature_extractor import FeatureExtractor
+from kimm.utils import make_divisible
+
+DEFAULT_CONFIG = [
+    # type, repeat, kernel_size, strides, expansion_ratio, channels
+    ["ds", 1, 3, 1, 1, 16],
+    ["ir", 2, 3, 2, 6, 24],
+    ["ir", 2, 5, 2, 6, 40],
+    ["ir", 3, 3, 2, 6, 80],
+    ["ir", 3, 5, 1, 6, 112],
+    ["ir", 4, 5, 2, 6, 192],
+    ["ir", 1, 3, 1, 6, 320],
+]
+
+
+def apply_depthwise_separation_block(
+    inputs,
+    output_channels,
+    depthwise_kernel_size=3,
+    pointwise_kernel_size=1,
+    strides=1,
+    activation="relu6",
+    bn_epsilon=1e-5,
+    padding=None,
+    name="depthwise_separation_block",
+):
+    input_channels = inputs.shape[-1]
+    has_skip = strides == 1 and input_channels == output_channels
+
+    x = inputs
+    x = apply_conv2d_block(
+        x,
+        kernel_size=depthwise_kernel_size,
+        strides=strides,
+        activation=activation,
+        use_depthwise=True,
+        bn_epsilon=bn_epsilon,
+        padding=padding,
+        name=f"{name}_conv_dw",
+    )
+    x = apply_conv2d_block(
+        x,
+        output_channels,
+        pointwise_kernel_size,
+        1,
+        activation=None,
+        bn_epsilon=bn_epsilon,
+        padding=padding,
+        name=f"{name}_conv_pw",
+    )
+    if has_skip:
+        x = layers.Add()([x, inputs])
+    return x
+
+
+def apply_inverted_residual_block(
+    inputs,
+    output_channels,
+    depthwise_kernel_size=3,
+    expansion_kernel_size=1,
+    pointwise_kernel_size=1,
+    strides=1,
+    expansion_ratio=1.0,
+    activation="relu6",
+    bn_epsilon=1e-5,
+    padding=None,
+    name="inverted_residual_block",
+):
+    input_channels = inputs.shape[-1]
+    hidden_channels = make_divisible(input_channels * expansion_ratio)
+    has_skip = strides == 1 and input_channels == output_channels
+
+    x = inputs
+
+    # Point-wise expansion
+    x = apply_conv2d_block(
+        x,
+        hidden_channels,
+        expansion_kernel_size,
+        1,
+        activation=activation,
+        bn_epsilon=bn_epsilon,
+        padding=padding,
+        name=f"{name}_conv_pw",
+    )
+    # Depth-wise convolution
+    x = apply_conv2d_block(
+        x,
+        kernel_size=depthwise_kernel_size,
+        strides=strides,
+        activation=activation,
+        use_depthwise=True,
+        bn_epsilon=bn_epsilon,
+        padding=padding,
+        name=f"{name}_conv_dw",
+    )
+    # Point-wise linear projection
+    x = apply_conv2d_block(
+        x,
+        output_channels,
+        pointwise_kernel_size,
+        1,
+        activation=None,
+        bn_epsilon=bn_epsilon,
+        padding=padding,
+        name=f"{name}_conv_pwl",
+    )
+    if has_skip:
+        x = layers.Add()([x, inputs])
+    return x
+
+
+class EfficientNetLite(FeatureExtractor):
+    def __init__(
+        self,
+        width: float = 1.0,
+        depth: float = 1.0,
+        fix_stem_and_head_channels: bool = False,
+        input_tensor: keras.KerasTensor = None,
+        input_shape: typing.Optional[typing.Sequence[int]] = None,
+        include_preprocessing: bool = True,
+        include_top: bool = True,
+        pooling: typing.Optional[str] = None,
+        dropout_rate: float = 0.0,
+        classes: int = 1000,
+        classifier_activation: str = "softmax",
+        weights: typing.Optional[str] = None,  # TODO: imagenet
+        config: typing.Union[str, typing.List] = "default",
+        **kwargs,
+    ):
+        if config == "default":
+            config = DEFAULT_CONFIG
+        # TF default config
+        default_size = kwargs.pop("default_size", 224)
+        bn_epsilon = kwargs.pop("bn_epsilon", 1e-5)
+        padding = kwargs.pop("padding", None)
+
+        # Prepare feature extraction
+        features = {}
+
+        # Determine proper input shape
+        input_shape = imagenet_utils.obtain_input_shape(
+            input_shape,
+            default_size=default_size,
+            min_size=32,
+            data_format=backend.image_data_format(),
+            require_flatten=include_top,
+            weights=weights,
+        )
+
+        if input_tensor is None:
+            img_input = layers.Input(shape=input_shape)
+        else:
+            if not backend.is_keras_tensor(input_tensor):
+                img_input = layers.Input(tensor=input_tensor, shape=input_shape)
+            else:
+                img_input = input_tensor
+
+        x = img_input
+
+        # [0, 255] to [0, 1] and apply ImageNet mean and variance
+        if include_preprocessing:
+            x = layers.Rescaling(scale=1.0 / 255.0)(x)
+            x = layers.Normalization(
+                mean=[0.485, 0.456, 0.406], variance=[0.229, 0.224, 0.225]
+            )(x)
+
+        # stem
+        stem_channel = 32
+        x = apply_conv2d_block(
+            x,
+            stem_channel,
+            3,
+            2,
+            activation="relu6",
+            bn_epsilon=bn_epsilon,
+            padding=padding,
+            name="conv_stem",
+        )
+        features["STEM_S2"] = x
+
+        # blocks
+        current_stride = 2
+        for current_block_idx, cfg in enumerate(config):
+            block_type, r, k, s, e, c = cfg
+            c = make_divisible(c * width)
+            # no depth multiplier at first and last block
+            if current_block_idx not in (0, len(config) - 1):
+                r = int(math.ceil(r * depth))
+            for current_layer_idx in range(r):
+                s = s if current_layer_idx == 0 else 1
+                common_kwargs = {
+                    "bn_epsilon": bn_epsilon,
+                    "padding": padding,
+                    "name": f"blocks_{current_block_idx}_{current_layer_idx}",
+                }
+                if block_type == "ds":
+                    x = apply_depthwise_separation_block(
+                        x, c, k, 1, s, "relu6", **common_kwargs
+                    )
+                elif block_type == "ir":
+                    x = apply_inverted_residual_block(
+                        x, c, k, 1, 1, s, e, "relu6", **common_kwargs
+                    )
+                elif block_type == "cn":
+                    x = apply_conv2d_block(
+                        x,
+                        filters=c,
+                        kernel_size=k,
+                        strides=s,
+                        activation="relu6",
+                        **common_kwargs,
+                    )
+                current_stride *= s
+            features[f"BLOCK{current_block_idx}_S{current_stride}"] = x
+
+        # last conv
+        conv_head_channels = 1280
+        x = apply_conv2d_block(
+            x,
+            conv_head_channels,
+            1,
+            1,
+            activation="relu6",
+            bn_epsilon=bn_epsilon,
+            name="conv_head",
+        )
+
+        if include_top:
+            x = layers.GlobalAveragePooling2D(name="avg_pool")(x)
+            x = layers.Dropout(rate=dropout_rate, name="conv_head_dropout")(x)
+            x = layers.Dense(
+                classes, activation=classifier_activation, name="classifier"
+            )(x)
+        else:
+            if pooling == "avg":
+                x = layers.GlobalAveragePooling2D(name="avg_pool")(x)
+            elif pooling == "max":
+                x = layers.GlobalMaxPooling2D(name="max_pool")(x)
+
+        # Ensure that the model takes into account
+        # any potential predecessors of `input_tensor`.
+        if input_tensor is not None:
+            inputs = utils.get_source_inputs(input_tensor)
+        else:
+            inputs = img_input
+
+        super().__init__(inputs=inputs, outputs=x, features=features, **kwargs)
+
+        # All references to `self` below this line
+        self.width = width
+        self.depth = depth
+        self.fix_stem_and_head_channels = fix_stem_and_head_channels
+        self.include_preprocessing = include_preprocessing
+        self.include_top = include_top
+        self.pooling = pooling
+        self.dropout_rate = dropout_rate
+        self.classes = classes
+        self.classifier_activation = classifier_activation
+        self._weights = weights  # `self.weights` is been used internally
+        self.config = config
+
+    @staticmethod
+    def available_feature_keys():
+        feature_keys = ["STEM_S2"]
+        feature_keys.extend(
+            [
+                f"BLOCK{i}_S{j}"
+                for i, j in zip(range(7), [2, 4, 8, 16, 16, 32, 32])
+            ]
+        )
+        return feature_keys
+
+    def get_config(self):
+        config = super().get_config()
+        config.update(
+            {
+                "width": self.width,
+                "input_shape": self.input_shape[1:],
+                "include_preprocessing": self.include_preprocessing,
+                "include_top": self.include_top,
+                "pooling": self.pooling,
+                "dropout_rate": self.dropout_rate,
+                "classes": self.classes,
+                "classifier_activation": self.classifier_activation,
+                "weights": self._weights,
+                "config": self.config,
+            }
+        )
+        return config
+
+
+"""
+Model Definition
+"""
+
+
+class EfficientNetLiteB0(EfficientNetLite):
+    def __init__(
+        self,
+        input_tensor: keras.KerasTensor = None,
+        input_shape: typing.Optional[typing.Sequence[int]] = None,
+        include_preprocessing: bool = True,
+        include_top: bool = True,
+        pooling: typing.Optional[str] = None,
+        dropout_rate: float = 0.0,
+        classes: int = 1000,
+        classifier_activation: str = "softmax",
+        weights: typing.Optional[str] = None,  # TODO: imagenet
+        config: typing.Union[str, typing.List] = "default",
+        name: str = "EfficientNetLiteB0",
+        **kwargs,
+    ):
+        # default to TF configuration (bn_epsilon=1e-3 and padding="same")
+        super().__init__(
+            1.0,
+            1.0,
+            False,
+            input_tensor,
+            input_shape,
+            include_preprocessing,
+            include_top,
+            pooling,
+            dropout_rate,
+            classes,
+            classifier_activation,
+            weights,
+            config,
+            name=name,
+            default_size=224,
+            bn_epsilon=1e-3,
+            padding="same",
+            **kwargs,
+        )
+
+
+class EfficientNetLiteB1(EfficientNetLite):
+    def __init__(
+        self,
+        input_tensor: keras.KerasTensor = None,
+        input_shape: typing.Optional[typing.Sequence[int]] = None,
+        include_preprocessing: bool = True,
+        include_top: bool = True,
+        pooling: typing.Optional[str] = None,
+        dropout_rate: float = 0.0,
+        classes: int = 1000,
+        classifier_activation: str = "softmax",
+        weights: typing.Optional[str] = None,  # TODO: imagenet
+        config: typing.Union[str, typing.List] = "default",
+        name: str = "EfficientNetLiteB1",
+        **kwargs,
+    ):
+        # default to TF configuration (bn_epsilon=1e-3 and padding="same")
+        super().__init__(
+            1.0,
+            1.1,
+            True,
+            input_tensor,
+            input_shape,
+            include_preprocessing,
+            include_top,
+            pooling,
+            dropout_rate,
+            classes,
+            classifier_activation,
+            weights,
+            config,
+            name=name,
+            default_size=240,
+            bn_epsilon=1e-3,
+            padding="same",
+            **kwargs,
+        )
+
+
+class EfficientNetLiteB2(EfficientNetLite):
+    def __init__(
+        self,
+        input_tensor: keras.KerasTensor = None,
+        input_shape: typing.Optional[typing.Sequence[int]] = None,
+        include_preprocessing: bool = True,
+        include_top: bool = True,
+        pooling: typing.Optional[str] = None,
+        dropout_rate: float = 0.0,
+        classes: int = 1000,
+        classifier_activation: str = "softmax",
+        weights: typing.Optional[str] = None,  # TODO: imagenet
+        config: typing.Union[str, typing.List] = "default",
+        name: str = "EfficientNetLiteB2",
+        **kwargs,
+    ):
+        # default to TF configuration (bn_epsilon=1e-3 and padding="same")
+        super().__init__(
+            1.1,
+            1.2,
+            True,
+            input_tensor,
+            input_shape,
+            include_preprocessing,
+            include_top,
+            pooling,
+            dropout_rate,
+            classes,
+            classifier_activation,
+            weights,
+            config,
+            name=name,
+            default_size=260,
+            bn_epsilon=1e-3,
+            padding="same",
+            **kwargs,
+        )
+
+
+class EfficientNetLiteB3(EfficientNetLite):
+    def __init__(
+        self,
+        input_tensor: keras.KerasTensor = None,
+        input_shape: typing.Optional[typing.Sequence[int]] = None,
+        include_preprocessing: bool = True,
+        include_top: bool = True,
+        pooling: typing.Optional[str] = None,
+        dropout_rate: float = 0.0,
+        classes: int = 1000,
+        classifier_activation: str = "softmax",
+        weights: typing.Optional[str] = None,  # TODO: imagenet
+        config: typing.Union[str, typing.List] = "default",
+        name: str = "EfficientNetLiteB3",
+        **kwargs,
+    ):
+        # default to TF configuration (bn_epsilon=1e-3 and padding="same")
+        super().__init__(
+            1.2,
+            1.4,
+            True,
+            input_tensor,
+            input_shape,
+            include_preprocessing,
+            include_top,
+            pooling,
+            dropout_rate,
+            classes,
+            classifier_activation,
+            weights,
+            config,
+            name=name,
+            default_size=300,
+            bn_epsilon=1e-3,
+            padding="same",
+            **kwargs,
+        )
+
+
+class EfficientNetLiteB4(EfficientNetLite):
+    def __init__(
+        self,
+        input_tensor: keras.KerasTensor = None,
+        input_shape: typing.Optional[typing.Sequence[int]] = None,
+        include_preprocessing: bool = True,
+        include_top: bool = True,
+        pooling: typing.Optional[str] = None,
+        dropout_rate: float = 0.0,
+        classes: int = 1000,
+        classifier_activation: str = "softmax",
+        weights: typing.Optional[str] = None,  # TODO: imagenet
+        config: typing.Union[str, typing.List] = "default",
+        name: str = "EfficientNetLiteB4",
+        **kwargs,
+    ):
+        # default to TF configuration (bn_epsilon=1e-3 and padding="same")
+        super().__init__(
+            1.4,
+            1.8,
+            True,
+            input_tensor,
+            input_shape,
+            include_preprocessing,
+            include_top,
+            pooling,
+            dropout_rate,
+            classes,
+            classifier_activation,
+            weights,
+            config,
+            name=name,
+            default_size=380,
+            bn_epsilon=1e-3,
+            padding="same",
+            **kwargs,
+        )

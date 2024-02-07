73616816fd57c571bfeb54e84dbe3f449740ef38
Hongyu Chiu <20734616+james77777778@users.noreply.github.com>
2024-02-07 23:19:25 +0800

    Update version

--- kimm/__init__.py
+++ kimm/__init__.py
@@ -1,6 +1,6 @@
 from kimm import export
-from kimm import models  # force to add models to the registry
+from kimm import models
 from kimm import utils
 from kimm.utils.model_registry import list_models
 
-__version__ = "0.1.7"
+__version__ = "0.1.8"
